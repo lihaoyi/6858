88a648b2481bf881b33926eb94956026ab93eff0
Li Haoyi <haoyi.sg@gmail.com>
2012-11-20 18:12:28 -0500

    Created the src/safelib folder, which contains the source code for our safe classes e.g. "safe.java.lang.System"

---
+++ src/safelib/safe/java/lang/System.java
@@ -0,0 +1,11 @@
+package safe.java.lang;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Haoyi
+ * Date: 11/20/12
+ * Time: 6:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class System {
+}
