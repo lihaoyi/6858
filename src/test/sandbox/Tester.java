f328044c5985bf684c7b5bc97efab4bbb0154e38
Li Haoyi <haoyi.sg@gmail.com>
2012-12-09 21:29:02 -0500

    Refactored out load-compile-instantiate-execute workflow into reusable metod

--- src/test/sandbox/Tester.java
+++ src/test/sandbox/Tester.java
@@ -18,20 +18,17 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 
 public class Tester {
-    public static void main(String[] args) throws Exception {
-
-	final String classname = "HelloWorld";
-
+    public static <T> T run (final String className, final int maxBytecodes, final int maxMemory) throws Exception{
         System.out.println("Loading Source...");
-        final String sourceCode = loadFile("resources/"+classname+".java");
+        final String sourceCode = loadFile("resources/"+className+".java");
 
         System.out.println("Compiling...");
-        final byte[] byteCode = Compiler.compile(classname, sourceCode);
+        final byte[] byteCode = Compiler.compile(className, sourceCode);
 
         System.out.println("Setting up Classloader...");
 
         MyClassLoader bcl = new MyClassLoader(new HashMap<String, byte[]>() {{
-            put(classname, byteCode);
+            put(className, byteCode);
         }});
 
         System.setProperty("java.security.policy", "resources/Test.policy");
@@ -43,17 +40,22 @@ public class Tester {
         System.out.println("Executing Code...");
         System.out.println("============================================");
 
-        Object key = Account.get().push(500000, 50000000L);
+        Object key = Account.get().push(maxMemory, maxBytecodes);
         Account.bcl = bcl; // TODO(TFK): Remove this hack.
 
-        Class c = bcl.loadClass(classname);
+        Class c = bcl.loadClass(className);
         Method m = c.getMethod("main");
-        String result = (String) m.invoke(null);
+        T result = (T) m.invoke(null);
 
         System.out.println("RESULT: " + result);
 
         Account.get().pop(key);
         System.out.println("============================================");
+
+        return result;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(run("HelloWorld", 50000, 50000000));
 
     }
 
