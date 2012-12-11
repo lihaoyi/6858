90571e320342045ed90fdeaea7b2281b738e367c
Frank Li <frankli@frankli.(none)>
2012-12-11 16:57:40 -0500

    Added passing unit test, set up tester for demo

---
+++ resources/ScriptsGood.java
@@ -0,0 +1,28 @@
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+/**
+ * This is an infinite loop, but creatd in Javascript using Rhino, rather
+ * than in plain Java. Because the JS interpreter also gets instrumented,
+ * the infinite loop in interpreting the Javascript also throws a
+ * ResourceLimitException to return control back to the trusted code.
+ */
+public class ScriptsGood {
+    public static String main() throws Exception {
+
+        ScriptEngineManager engineMgr = new ScriptEngineManager();
+        ScriptEngine engine = engineMgr.getEngineByName("JavaScript");
+        Bindings bindings = engine.createBindings();
+
+        String script =
+                "(function(){" +
+                    "var x = 2; " +
+                    "while (x < 3) {" +
+                      "x = x + 1" +
+                    "}" +
+                "})()";
+        engine.eval(script);
+        return "ScriptsGood returns correctly!";
+    }
+}
